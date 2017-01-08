16e41b060c94c6bfeb90aeaa7f95345dcdd8e61d
Ben Crist <ben@magicmoremagic.com>
2017-01-08 15:59:55 -0600

    build.lua syntax overhaul

--- build.lua
+++ build.lua
@@ -1,27 +1,18 @@
-tool { name = 'limp',
-   projects = {
-      app {
-         deploy_bin = true,
-         icon = 'icon/bengine-warm.ico',
-         limp = {
-            file = 'include/limp_lua.hpp',
-            inputs = { 'meta/limp.lua' }
-         },
-         src = {
-            'src/*.cpp'
-         },
-         libs = {
-            'core',
-            'core-id',
-            'util',
-            'ctable',
-            'cli',
-            'blt',
-            'luacore',
-            'luautil',
-            'luablt',
-            'zlib-static'
-         }
+tool 'limp' {
+   app {
+      icon 'icon/bengine-warm.ico',
+      limp { file = 'include/limp_lua.hpp', inputs = 'meta/limp.lua' },
+      link_project {
+         'core',
+         'core-id',
+         'util',
+         'ctable',
+         'cli',
+         'blt',
+         'luacore',
+         'luautil',
+         'luablt',
+         'zlib-static'
       }
    }
 }
