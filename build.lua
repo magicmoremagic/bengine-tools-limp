9bb0539011d64ff4c2d286a1897e79bebcb41d25
Ben Crist <ben@magicmoremagic.com>
2017-01-10 16:37:52 -0600

    link_project deps

--- build.lua
+++ build.lua
@@ -3,16 +3,11 @@ tool 'limp' {
       icon 'icon/bengine-warm.ico',
       limp { file = 'include/limp_lua.hpp', inputs = 'meta/limp.lua' },
       link_project {
-         'core',
-         'core-id',
-         'util',
-         'ctable',
+         'core-id-with-names',
          'cli',
          'blt',
-         'luacore',
          'luautil',
-         'luablt',
-         'zlib-static'
+         'luablt'
       }
    }
 }
