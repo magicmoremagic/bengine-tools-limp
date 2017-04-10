b5869d74e75fdfff5731bfe4fc0390a4b5a258f7
Ben Crist <ben@magicmoremagic.com>
2017-04-09 19:18:05 -0500

    vcxproj

--- include/version.hpp
+++ include/version.hpp
@@ -6,7 +6,7 @@
 
 #define BE_LIMP_VERSION_MAJOR 0
 #define BE_LIMP_VERSION_MINOR 1
-#define BE_LIMP_VERSION_REV 2
+#define BE_LIMP_VERSION_REV 3
 
 /*!! include('common/version', 'BE_LIMP', 'LIMP') !! 6 */
 /* ################# !! GENERATED CODE -- DO NOT MODIFY !! ################# */
